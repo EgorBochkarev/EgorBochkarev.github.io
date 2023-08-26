8c7cf0dd1e6d80af8a2c96dfdc3a985c84edce37
EgorBochkarev <bochkarevei@gmail.com>
2023-08-27 01:37:56 +0300

    Publish virtualized_list

--- advanced_js/allKeysAndSymbols.js
+++
@@ -1,7 +0,0 @@
-
-function allKeysAndSymbols (object) {
-    const prototype = Object.getPrototypeOf(object);
-    return [...Object.getOwnPropertyNames(object), ...Object.getOwnPropertySymbols(object), ...(prototype ? allKeysAndSymbols(prototype) : [])];
-}
-
-console.log(allKeysAndSymbols({}));
